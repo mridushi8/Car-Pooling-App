45544c123989bc2ed6d2ac8b40a770ab195b7b2a
Arushi Butan <arushibutan11@gmail.com>
2017-01-30 01:34:29 +0530

    Added session variables

--- app/src/main/java/com/example/arushi/login/MainActivity.java
+++ app/src/main/java/com/example/arushi/login/MainActivity.java
@@ -1,25 +1,15 @@
 package com.example.arushi.login;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.String;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Button;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.io.*;
-import java.net.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.lang.*;
-import android.view.View;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -29,17 +19,30 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static android.widget.Toast.LENGTH_LONG;
-import static org.apache.http.protocol.HTTP.USER_AGENT;
 
 
 public class MainActivity extends AppCompatActivity {
     EditText email, pass;
     String emailid, Pass;
+    public static final String MyPREFERENCES = "MyPref" ;
+    public static final String Name = "nameKey";
+    public static final String Userid = "idKey";
+    SharedPreferences sharedpreferences;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+
         if (android.os.Build.VERSION.SDK_INT > 9)
         {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -52,16 +55,19 @@ public class MainActivity extends AppCompatActivity {
             final TextView signup =(TextView)findViewById(R.id.reg);
         signup.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v) {
-
                 Intent i = new Intent(MainActivity.this, Register.class);
-                startActivity(i);
+                                   startActivity(i);
+
             }
         });
+
         //Submit Button for login
         Button saveme=(Button)findViewById(R.id.save);
         saveme.setOnClickListener(new Button.OnClickListener(){
 
             public void onClick(View v) {
+
+
 
                 //to check if any one of them is NULL
                 if(email.length()==0 || pass.length()==0){
@@ -70,9 +76,9 @@ public class MainActivity extends AppCompatActivity {
 
                 }
                 //to check if pw is less than 8 characters
-                else if(pass.length() <= 8){
+                else if(pass.length() < 8){
 
-                    Toast.makeText(getApplicationContext(), "Invalid Input!", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Password must be more than 8 characters!", Toast.LENGTH_LONG).show();
 
                 }
                 //to check if email contains @
@@ -86,23 +92,58 @@ public class MainActivity extends AppCompatActivity {
                 Pass = pass.getText().toString();
 
                 RequestQueue MyRequestQueue = Volley.newRequestQueue(getBaseContext());
-
-                String url= R.string.serverAddress + "/checkmatch";
+                String url= "http://192.168.1.3:8000/checkmatch/";
                 StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>(){
                     @Override
                     public void onResponse(String response) {
                         Context context = getApplicationContext();
-                        Toast.makeText(context, R.string.popup, LENGTH_LONG)
-                                .show();
-
-                    }
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
 
 
-                }, new Response.ErrorListener() {
+                        if(response.equals("\"nomatch\""))
+                        {
+                           Toast.makeText(context, R.string.popup, LENGTH_LONG)
+                                   .show();
+                        }
+
+                       else if(response.equals("\"nouser\""))
+                        {
+                            Toast.makeText(context, "The user is not registered!", LENGTH_LONG)
+                                    .show();
+                        }
+                        else
+                        {  //retrieve the userid and name from server and save it in shared preferences
+
+                            String[] parts = response.split(",");
+                            String[] t = parts[0].split(":");
+                            String id=t[1];
+
+                            String[] t1 = parts[1].split(":");
+                            String name=t1[1];
+                            name = name.replace("}","");
+                            name = name.substring(1);
+                            name= name.substring(0,name.length() - 1);
+
+
+                            editor.putString(Userid, id);
+                            editor.putString(Name, name);
+                            editor.commit();
+
+                            //for retrieving data from shared preferences
+                            String value = sharedpreferences.getString(Userid, "");
+                            System.out.println(value);
+
+                            Intent i = new Intent(MainActivity.this, afterlogin.class);
+                            startActivity(i);
+
+
+                        }
+                        }
+                    }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Context context = getApplicationContext();
-                        Toast.makeText(context, R.string.fail, LENGTH_LONG)
+                       Toast.makeText(context, R.string.fail, LENGTH_LONG)
                                 .show();
                         error.printStackTrace();
                     }
@@ -113,15 +154,14 @@ public class MainActivity extends AppCompatActivity {
                         MyData.put("pass", Pass);
                         return MyData;
                     };
+
                 };
+
                 MyRequestQueue.add(MyStringRequest);
-                Intent i = new Intent(MainActivity.this, afterlogin.class);
-                startActivity(i);
+                }
 
 
-            }}
-
-        });
+            }});
 
 
 
