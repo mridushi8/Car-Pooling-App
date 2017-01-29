873f04605d4ec63c2de7cac38b240698149b3e3c
ARATI <arathi.n97@gmail.com>
2017-01-29 19:18:39 +0530

    Integrated Screen 4

--- app/src/main/java/com/example/arushi/login/afterlogin.java
+++ app/src/main/java/com/example/arushi/login/afterlogin.java
@@ -1,5 +1,6 @@
 package com.example.arushi.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class afterlogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_afterlogin);
+		Intent i = new Intent(afterlogin.this, MapsActivity.class);
+        startActivity(i);
     }
 }
